26386be14794f87151342b58fbf2dfaac169d250
christineZ5 <christinetechwork5@gmail.com>
2020-04-21 16:10:58 -0700

    added html and folders

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+/* Global variables */
+
+
+// Variables for the game
+let numWins = 0, numLosses = 0;
+
+// Variables for the user
+let answer;
+let guesses_array, guesses_string, numTriesLeft;
+
+
+
+/* Start a new game when the page loads for the first time */
+
+function startNewGame() {
+
+  
+}
